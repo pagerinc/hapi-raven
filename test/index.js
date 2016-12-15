1a4c18875d637f0bf8233f89bb35ef4f806385fd
Gaston Festari <cilindrox@gmail.com>
2016-12-15 18:11:15 -0300

    feat: update raven

--- test/index.js
+++ test/index.js
@@ -5,10 +5,7 @@
 const Lab = require('lab');
 const Code = require('code');
 
-const Boom = require('boom');
 const Hapi = require('hapi');
-const Raven = require('raven');
-const Sinon = require('sinon');
 const Plugin = require('../');
 
 
@@ -20,16 +17,8 @@ const internals = {};
 // Test shortcuts
 
 const lab = exports.lab = Lab.script();
-const afterEach = lab.afterEach;
 const expect = Code.expect;
 const it = lab.it;
-
-
-afterEach((done) => {
-
-    Raven.Client.restore();
-    done();
-});
 
 
 it('registers with the dsn and client options', (done) => {
@@ -37,27 +26,22 @@ it('registers with the dsn and client options', (done) => {
     const server = new Hapi.Server();
     server.connection();
 
-    Sinon.stub(Raven, 'Client', (opts) => {
-
-        expect(opts).to.equal('dsn');
-
-        return {
-            patchGlobal: (cb) => expect(cb).to.be.a.function()
-        };
-    });
-
     const plugins = {
         register: Plugin,
         options: {
-            dsn: 'dsn',
+            dsn: 'https://public:private@app.getsentry.com/269dsn',
             patchGlobal: true,
-            client: Raven
+            client: { foo: 'bar' }
         }
     };
 
     server.register(plugins, (err) => {
 
         expect(err).to.not.exist();
+        const client = server.plugins.raven;
+        expect(client).to.be.an.object();
+        expect(client.raven.raw_dsn).to.equal('https://public:private@app.getsentry.com/269dsn');
+        expect(client.raven.installed).to.be.true();
         return done();
     });
 });
@@ -65,31 +49,13 @@ it('registers with the dsn and client options', (done) => {
 
 it('captures a request-error', (done) => {
 
-    Sinon.stub(Raven, 'Client', (opts) => {
-
-        return {
-            captureError: function (ignore, params) {
-
-                return {
-                    timestamp: Sinon.match.number,
-                    id: Sinon.match.string,
-                    method: 'get',
-                    path: '/',
-                    query: {},
-                    remoteAddress: '127.0.0.1',
-                    userAgent: 'shot'
-                };
-            }
-        };
-    });
-
     const server = new Hapi.Server();
     server.connection();
 
     const plugins = {
         register: Plugin,
         options: {
-            dsn: 'dsn',
+            dsn: 'https://public:private@app.getsentry.com/269dsn',
             client: {}
         }
     };
@@ -97,7 +63,6 @@ it('captures a request-error', (done) => {
     server.register(plugins, (err) => {
 
         expect(err).to.not.exist();
-
         server.route({
             method: 'GET',
             path: '/',
@@ -108,76 +73,5 @@ it('captures a request-error', (done) => {
 
             return done();
         });
-    });
-});
-
-
-it('does not capture Boom errors', (done) => {
-
-    Sinon.stub(Raven, 'Client', (opts) => {
-
-        return {
-            captureError: () => {
-
-                throw new Error('shouldn\'t have been called!');
-            }
-        };
-    });
-
-    const server = new Hapi.Server();
-    server.connection();
-
-    const plugins = {
-        register: Plugin,
-        options: {
-            dsn: 'dsn',
-            client: {}
-        }
-    };
-
-    server.register(plugins, (err) => {
-
-        expect(err).to.not.exist();
-
-        server.route({
-            method: 'GET',
-            path: '/boom',
-            handler: (request, reply) => reply(Boom.forbidden())
-        });
-
-        server.inject('/boom', () => {
-
-            return done();
-        });
-    });
-});
-
-
-it('does not patch global if missing dsn', (done) => {
-
-    const server = new Hapi.Server();
-    server.connection();
-
-    Sinon.stub(Raven, 'Client', (opts) => {
-
-        expect(opts).to.not.exist();
-
-        return {
-            patchGlobal: (cb) => expect(cb).to.not.exist()
-        };
-    });
-
-    const plugins = {
-        register: Plugin,
-        options: {
-            patchGlobal: true,
-            client: Raven
-        }
-    };
-
-    server.register(plugins, (err) => {
-
-        expect(err).to.not.exist();
-        return done();
     });
 });
