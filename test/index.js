68fac6283e6c221d89e68e923e14069dec17bb55
David Roncancio <david.roncancio@gmail.com>
2018-01-30 08:50:34 -0600

    feat: hapi v17

--- test/index.js
+++ test/index.js
@@ -21,57 +21,46 @@ const expect = Code.expect;
 const it = lab.it;
 
 
-it('registers with the dsn and client options', (done) => {
+it('registers with the dsn and client options', async () => {
 
-    const server = new Hapi.Server();
-    server.connection();
+    const server = await new Hapi.Server();
 
-    const plugins = {
-        register: Plugin,
+    await server.register({
+        plugin: Plugin,
         options: {
             dsn: 'https://public:private@app.getsentry.com/269dsn',
             patchGlobal: true,
             client: { foo: 'bar' }
         }
-    };
-
-    server.register(plugins, (err) => {
-
-        expect(err).to.not.exist();
-        const client = server.plugins.raven;
-        expect(client).to.be.an.object();
-        expect(client.raven.raw_dsn).to.equal('https://public:private@app.getsentry.com/269dsn');
-        expect(client.raven.installed).to.be.true();
-        return done();
     });
+
+    const client = server.plugins[process.env.npm_package_name];
+    expect(client).to.be.an.object();
+    expect(client.raven.raw_dsn).to.equal('https://public:private@app.getsentry.com/269dsn');
+    expect(client.raven.installed).to.be.true();
 });
 
 
-it('captures a request-error', (done) => {
+it('captures a request-error', async () => {
 
-    const server = new Hapi.Server();
-    server.connection();
+    const server = await new Hapi.Server();
 
-    const plugins = {
-        register: Plugin,
+    await server.register({
+        plugin: Plugin,
         options: {
             dsn: 'https://public:private@app.getsentry.com/269dsn',
-            client: {}
+            client: { }
         }
-    };
-
-    server.register(plugins, (err) => {
-
-        expect(err).to.not.exist();
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: (request, reply) => reply(new Error())
-        });
-
-        server.inject('/', () => {
-
-            return done();
-        });
     });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, reply) => {
+
+            throw new Error();
+        }
+    });
+
+    await server.inject('/');
 });
